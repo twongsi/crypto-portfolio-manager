490a9359a3748fddc9327d84caa4a9d468a8b728
Aaron Mamparo <aaronmamparo@gmail.com>
2021-01-30 21:17:30 -0600

    break things out to debug

--- aws/main.py
+++ aws/main.py
@@ -14,35 +14,40 @@ def env(key: str) -> str:
 class MainStack(Stack):
     def __init__(self, scope: Construct, _id: str, **kwargs) -> None:
         super().__init__(scope, _id, **kwargs)
+        schedule = Schedule.cron(
+            minute='0',
+            hour='2',
+            week_day='MON'
+        )
+        image = ContainerImage.from_asset(
+            os.getcwd(),
+            file='Dockerfile',
+            repository_name='crypto-portfolio-manager',
+            exclude=['cdk.out']
+        )
+        log_group = LogGroup(self, 'CryptoPortfolioManagerLogGroup')
+        log_driver = AwsLogDriver(
+            stream_prefix='crypto-portfolio-manager',
+            log_group=log_group
+        )
+        scheduled_fargate_task_image_options = ScheduledFargateTaskImageOptions(
+            image=image,
+            command=['pipenv', 'run', 'python', '-u', '-m', 'src.main'],
+            environment={
+                'COINBASE_PRO_API_PASSPHRASE': env('COINBASE_PRO_API_PASSPHRASE'),
+                'COINBASE_PRO_API_SECRET': env('COINBASE_PRO_API_SECRET'),
+                'COINBASE_PRO_API_KEY': env('COINBASE_PRO_API_KEY'),
+                'N_PORTFOLIO_HOLDINGS': 5
+            },
+            log_driver=log_driver,
+            cpu=256,
+            memory_limit_mib=512
+        )
         ScheduledFargateTask(
             self,
             'ScheduledTask',
-            schedule=Schedule.cron(
-                minute='0',
-                hour='2',
-                week_day='MON'
-            ),
-            scheduled_fargate_task_image_options=ScheduledFargateTaskImageOptions(
-                image=ContainerImage.from_asset(
-                    os.getcwd(),
-                    file='Dockerfile',
-                    repository_name='crypto-portfolio-manager',
-                    exclude=['cdk.out']
-                ),
-                command=['pipenv', 'run', 'python', '-u', '-m', 'src.main'],
-                environment={
-                    'COINBASE_PRO_API_PASSPHRASE': env('COINBASE_PRO_API_PASSPHRASE'),
-                    'COINBASE_PRO_API_SECRET': env('COINBASE_PRO_API_SECRET'),
-                    'COINBASE_PRO_API_KEY': env('COINBASE_PRO_API_KEY'),
-                    'N_PORTFOLIO_HOLDINGS': 5
-                },
-                log_driver=AwsLogDriver(
-                    stream_prefix='crypto-portfolio-manager',
-                    log_group=LogGroup(self, 'CryptoPortfolioManagerLogGroup')
-                ),
-                cpu=256,
-                memory_limit_mib=512
-            )
+            schedule=schedule,
+            scheduled_fargate_task_image_options=scheduled_fargate_task_image_options
         )
 
 
